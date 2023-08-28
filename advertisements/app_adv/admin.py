009b54b3499c84a15d79f17d3b2469a62006f8ef
Влад <milionerreayd@gmail.com>
2023-08-28 20:29:19 +0300

    Homework-9

--- advertisements/app_adv/admin.py
+++ advertisements/app_adv/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import Advertisement
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction']
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction', 'user', 'img']
     list_filter = ['auction', 'created_at', 'updated_at']
     actions = ['make_auction_as_false', 'make_auction_as_true']
     fieldsets = (
         ('Общее', {
-            'fields': ('title', 'description')
+            'fields': ('title', 'description', 'user', 'image')
         }),
         ('Финансы', {
             'fields': ('price', 'auction'),
--- advertisements/app_adv/models.py
+++ advertisements/app_adv/models.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.html import format_html
 from django.contrib import admin
+
+User=get_user_model()
+
 class Advertisement(models.Model):
     title = models.CharField('Заголовок', max_length=128)
     description = models.TextField('Описание')
@@ -8,6 +12,8 @@ class Advertisement(models.Model):
     auction = models.BooleanField('торг', help_text='Отметьте, если торг уместен')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    image=models.ImageField("Изображение", upload_to='advertisements/')
 
     @admin.display(description='Дата создания')
     def created_date(self):
@@ -23,6 +29,17 @@ class Advertisement(models.Model):
             updated_time = self.updated_at.time().strftime('%H:%M:%S')
             return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', updated_time)
         return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='Изображения')
+    def img(self):
+        if self.image:
+            return format_html(
+                '<a href="{}" target="_blank">'
+                '<img alt="Card title" width="50" height="50" class="img-fluid rounded-start" src="{}">'
+                '</a>',
+                self.image.url, self.image.url)
+        else:
+            return format_html('<span style="color: red; font-weight: bold;">Изображение не найдено / Изображения нет</span>')
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
